4d1d2a2a5cc731685315cc6549a82e96dc10ec17
njhyuk <njh2239@gmail.com>
2021-01-17 15:09:47 +0900

    송장이 발행되지 않은 각 주문에 대한 송장이 반환되어야 한다.

--- specs/domain/service/invoice-factory.spec.php
+++ specs/domain/service/invoice-factory.spec.php
@@ -1,5 +1,8 @@
 <?php
 
+use CleanPhp\Invoicer\Domain\Entity\Invoice;
+use CleanPhp\Invoicer\Domain\Entity\Order;
+use CleanPhp\Invoicer\Domain\Factory\InvoiceFactory;
 use CleanPhp\Invoicer\Domain\Repository\OrderRepositoryInterface;
 use CleanPhp\Invoicer\Domain\Service\InvocingService;
 
@@ -7,15 +10,34 @@ describe('InvoiceService', function () {
     describe('->generateInvoices()', function () {
         beforeEach(function () {
             $this->repository = $this->getProphet()->prophesize(OrderRepositoryInterface::class);
+            $this->factory = $this->getProphet()->prophesize(InvoiceFactory::class);
         });
 
         it('저장소에 송장이 발행되지 않은 주문을 쿼리해야한다.', function () {
             $this->repository->getUninvoicedOrders()->shouldBeCalled();
-            $service = new InvocingService($this->repository->reveal());
+            $service = new InvocingService(
+                $this->repository->reveal(),
+                $this->factory->reveal()
+            );
             $service->generateInvoices();
         });
 
-        it('송장이 발행되지 않은 각 주문에 대한 송장이 반환되어야 한다.');
+        it('송장이 발행되지 않은 각 주문에 대한 송장이 반환되어야 한다.', function () {
+            $orders = [(new Order())->setTotal(400)];
+            $invoices = [(new Invoice())->setTotal(400)];
+
+            $this->repository->getUninvoicedOrders()->willReturn($orders);
+            $this->factory->createFromOrder($orders[0])->willReturn($invoices[0]);
+
+            $service = new InvocingService(
+                $this->repository->reveal(),
+                $this->factory->reveal()
+            );
+            $results = $service->generateInvoices();
+
+            expect($results)->to->be->a('array');
+            expect($results)->to->have->length(count($orders));
+        });
 
         afterEach(function () {
             $this->getProphet()->checkPredictions();
--- src/Domain/Service/InvocingService.php
+++ src/Domain/Service/InvocingService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CleanPhp\Invoicer\Domain\Service;
 
+use CleanPhp\Invoicer\Domain\Factory\InvoiceFactory;
 use CleanPhp\Invoicer\Domain\Repository\OrderRepositoryInterface;
 
 class InvocingService
@@ -11,12 +12,24 @@ class InvocingService
     /** @var OrderRepositoryInterface  */
     private OrderRepositoryInterface $orderRepository;
 
-    public function __construct(OrderRepositoryInterface $orderRepository)
+    /** @var InvoiceFactory  */
+    private InvoiceFactory $invoiceFactory;
+
+    public function __construct(OrderRepositoryInterface $orderRepository, InvoiceFactory $invoiceFactory)
     {
         $this->orderRepository = $orderRepository;
+        $this->invoiceFactory = $invoiceFactory;
     }
 
-    public function generateInvoices() {
+    public function generateInvoices(): array {
         $orders = $this->orderRepository->getUninvoicedOrders();
+
+        $invoices = [];
+
+        foreach ($orders as $order) {
+            $invoices[] = $this->invoiceFactory->createFromOrder($order);
+        }
+
+        return $invoices;
     }
 }
